46863d0dc8bd45444b12c40b843ec965f36c601f
hassen <hmyws187404@gmail.com>
2023-07-19 12:15:23 +0300

    the first commit on this repository

---
+++ new.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello worldachin\n";
+    int x;
+    cout<<"enter the number you want find its square \n";
+    cin>>x;
+    cout<<"\n square  of "<< x<<" is "<<pow(x,2)<<endl;
+    return -1;
+}
